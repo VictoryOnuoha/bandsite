9ec219fe83c2a39482a5940c67bd3454b3620926
Victory Onuoha <Victoryonuoha@cmail.carleton.ca>
2022-06-15 20:17:42 -0400

    Added array for comments and display styling for form area

--- scripts/index-page.js
+++ scripts/index-page.js
@@ -5,7 +5,7 @@ const comments = [
         name: "Connor Walton",
         timestamp: "02/17/2021",
         // Do we create the date as a  
-        // string or a number?
+        // string or a number? You can use the date object
         comment: "This is art. This is inexplicable magic expressed in the purest way, everything that makes up this majestic work deserves reverence. Let us appreciate this for what it is and what it contains.",
         userAvatar: ""
     },
