7b6c5f4406d63fb00e717c4cc0e5b75dcd83e8ed
Victory Onuoha <Victoryonuoha@cmail.carleton.ca>
2022-06-22 21:52:18 -0400

    Modified axios promise to include functionality to display data using DOM

--- scripts/build-shows-page.js
+++ scripts/build-shows-page.js
@@ -1,63 +1,28 @@
 // Save apikey in a variable as per documentation
 // let apiKey = "185f52d0-af7b-4577-8b06-8db46e0375c7";
 
-// Get API to get Shows Data 
-axios
-.get("https://project-1-api.herokuapp.com/showdates?api_key=185f52d0-af7b-4577-8b06-8db46e0375c7")
-.then(response => {
-    console.log(response);
-})
-.catch(error => {
-    console.log(error)
-})
-
-// Array of Objects to hold shows data 
-const concerts = [
-    {
-        date: "Mon Sept 06 2021 ",
-        venue: "Ronald Lane ",
-        location: "San Francisco, CA"
-    },
-    {
-        date: "Tue Sept 21 2021",
-        venue: "Pier 3 East",
-        location: "San Francisco, CA"
-    },
-    {
-        date: "Fri Oct 15 2021",
-        venue: "View Lounge",
-        location: "San Francisco, CA"
-    },
-    {
-        date: "Sat Nov 06 2021",
-        venue: "Hyatt Agency",
-        location: "San Francisco, CA"
-    },
-    {
-        date: "Fri Nov 26 2021",
-        venue: "Moscow Center",
-        location: "San Francisco, CA"
-    },
-    {
-        date: "Wed Dec 15 2021",
-        venue: "Press Club",
-        location: "San Francisco, CA"
-    },
-];
-
-// console.table(concerts);
 
 const concertCard = document.querySelector(".shows");
+
 // Concert title element
 const concertTitle = document.createElement("h2");
 concertTitle.innerText = "Shows";
 concertTitle.classList.add("shows__title");
 concertCard.appendChild(concertTitle);
-// console.log(concertCard)
 
-function displayConcert(concert) {
+// Get API to get Shows Data 
+axios
+.get("https://project-1-api.herokuapp.com/showdates?api_key=185f52d0-af7b-4577-8b06-8db46e0375c7")
+.then(response => {
+    console.log(response);
 
-
+    //Extract data from API ; Single item for now; the right data was extracted
+    const dateData = response.data[0].date;
+    const venueData = response.data[0].place;
+    const locationData = response.data[0].location;
+    
+    // Display Data
+    
     //concert card element
     const showsCard = document.createElement("section");
     showsCard.classList.add("shows__card");
@@ -74,7 +39,8 @@ function displayConcert(concert) {
     singleCard.appendChild(singlecardDates);
     //date
     const singlecardDate = document.createElement("p");
-    singlecardDate.innerText = concert.date;
+    singlecardDate.innerText = dateData;
+    console.log(singlecardDate);
     singleCard.appendChild(singlecardDate);
 
     //single card venue 
@@ -89,7 +55,7 @@ function displayConcert(concert) {
 
     //venue
     const singlecardVenue = document.createElement("p");
-    singlecardVenue.innerText = concert.venue;
+    singlecardVenue.innerText = venueData;
     singleVenueCard.appendChild(singlecardVenue);
 
     //single card location
@@ -104,7 +70,7 @@ function displayConcert(concert) {
 
     //location
     const singlecardLocation = document.createElement("p");
-    singlecardLocation.innerText = concert.location;
+    singlecardLocation.innerText = locationData;
     singleLocationCard.appendChild(singlecardLocation);
 
     //button
@@ -113,9 +79,48 @@ function displayConcert(concert) {
     concertButton.innerText = "BUY TICKETS";
     showsCard.appendChild(concertButton);
 
-}
+})
+.catch(error => {
+    console.log(error)
+})
+
+
+
 //Loop through concerts array 
 for (let i = 0; i < concerts.length; i++) {
     displayConcert(concerts[i]);
 }
 
+// Array of Objects to hold shows data 
+// const concerts = [
+//     {
+//         date: "Mon Sept 06 2021 ",
+//         venue: "Ronald Lane ",
+//         location: "San Francisco, CA"
+//     },
+//     {
+//         date: "Tue Sept 21 2021",
+//         venue: "Pier 3 East",
+//         location: "San Francisco, CA"
+//     },
+//     {
+//         date: "Fri Oct 15 2021",
+//         venue: "View Lounge",
+//         location: "San Francisco, CA"
+//     },
+//     {
+//         date: "Sat Nov 06 2021",
+//         venue: "Hyatt Agency",
+//         location: "San Francisco, CA"
+//     },
+//     {
+//         date: "Fri Nov 26 2021",
+//         venue: "Moscow Center",
+//         location: "San Francisco, CA"
+//     },
+//     {
+//         date: "Wed Dec 15 2021",
+//         venue: "Press Club",
+//         location: "San Francisco, CA"
+//     },
+// ];
