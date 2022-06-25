e729e6dc03fc5ea7f4ff19f47aa368334e1a5d42
Victory Onuoha <Victoryonuoha@cmail.carleton.ca>
2022-06-25 09:05:41 -0400

    Converted timestamp data to day month year format via Date object and its methods

--- scripts/index-page.js
+++ scripts/index-page.js
@@ -1,6 +1,29 @@
 const commentEl = document.querySelector(".comment");
 
 // API from Comments Data to display default comments
+function getComments() {
+    axios
+.get("https://project-1-api.herokuapp.com/comments?api_key=185f52d0-af7b-4577-8b06-8db46e0375c7")
+.then(response => {
+    //Default Comment Array
+    const commentData = response.data.sort((a, b) => {
+        return b.timestamp - a.timestamp ;
+    });
+    console.log(response.data[0]);
+    console.log(commentData);
+
+    //Loop through comments array
+for (let i = 0; i < commentData.length; i++) {
+    displayComment(commentData[i]);
+    // console.log(displayComment);
+}
+})
+.catch(error => {
+    console.log(error);
+});
+}
+console.log(getComments());
+
 axios
 .get("https://project-1-api.herokuapp.com/comments?api_key=185f52d0-af7b-4577-8b06-8db46e0375c7")
 .then(response => {
@@ -9,7 +32,7 @@ axios
     console.log(response.data[0]);
     console.log(commentData);
 
-    //Loop through comments array 
+    //Loop through comments array
 for (let i = 0; i < commentData.length; i++) {
     displayComment(commentData[i]);
     // console.log(displayComment);
@@ -19,30 +42,33 @@ for (let i = 0; i < commentData.length; i++) {
     console.log(error);
 });
 
-   
 
 
-// Function to display one commment object 
+// Function to display one commment object
 function displayComment(comment) {
-    // Extract Data 
+    // Extract Data
     const nameData = comment.name;
+
     const timestampData = comment.timestamp;
+    const timestampDateObj = new Date (timestampData);
+    const convertedTimestamp = timestampDateObj.toLocaleDateString(undefined);
+    
     const commentData = comment.comment;
-  
-   
+    
     // comment card element
     const commentCard = document.createElement("section");
     commentCard.classList.add("comment-card");
 
-    //  create image container 
+    //  create image container
     const imageContent = document.createElement("article");
     imageContent.classList.add("comment-container1");
     commentCard.appendChild(imageContent);
 
     // Comment Image
     const commentImage = document.createElement("img");
-    commentImage.setAttribute("src", "../assets/images/comment-avatar");
     commentImage.classList.add("comment-avatar");
+     commentImage.setAttribute("src", "./assets/images/comment-avatar.jpg");
+     console.log(commentImage);
     imageContent.appendChild(commentImage);
 
     // Comment container
@@ -59,10 +85,10 @@ function displayComment(comment) {
     //timestamp
     const commentTime = document.createElement("span");
     commentTime.classList.add("comment-container__timestamp");
-    commentTime.innerText = timestampData;
+    commentTime.innerText = convertedTimestamp;
     commentContent.appendChild(commentTime);
 
-    //comment 
+    //comment
     const commentText = document.createElement("p");
     commentText.classList.add("comment-container__comment");
     commentText.innerText = commentData;
@@ -71,9 +97,9 @@ function displayComment(comment) {
     commentEl.appendChild(commentCard);
 }
 
-//Create new comments 
+//Create new comments
 let formElement = document.querySelector(".form");
-    //Add event listener 
+    //Add event listener
     formElement.addEventListener("submit", (event) => {
         event.preventDefault();
         const newComment = {
@@ -81,13 +107,18 @@ let formElement = document.querySelector(".form");
             comment: event.target.userComment.value
         };
 
-    // Post new Comments 
+
+    // Post new Comments
     axios
     .post("https://project-1-api.herokuapp.com/comments?api_key=185f52d0-af7b-4577-8b06-8db46e0375c7", newComment)
     .then(response => {
-        newCommentData = response.data
-        displayComment(newCommentData);
+    //     newCommentData = response.data
+    //    commentData.unshift(newCommentData);
+        // displayComment(newCommentData);
+        getComments();
+        console.log(response.data);
     })
+    console.log(newCommentData);
     formElement.reset();
 })
 
@@ -100,24 +131,18 @@ let formElement = document.querySelector(".form");
 //     // select inputs by value property
 //     const fullName = event.target.fullName.value;
 //     const userComment = event.target.userComment.value;
-   
+
 //     newComment.name = fullName;
 //     newComment.comment = userComment;
-    
-    
+
 //     comments.unshift(newComment);
 
 //     submitForm(comments);
-    
-
 
 // });
 
-
-
-
 // const submitForm = userValue => {
-    
+
 //     for (let i = 0; i < userValue.length; i++) {
 //         const newComment = document.createElement("p");
 //         newComment.innerText = userValue[i].name;
